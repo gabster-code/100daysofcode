d7eea07429444d95e4bf070965e29a1febb550f6
gabster-code <88597385+gabster-code@users.noreply.github.com>
2022-08-27 21:41:42 +0800

    Started Sophomore Track
    Day 2 -Progress
    Started Sophomore Track

---
+++ Activity Progress/Day 2/Intro To React/SearchToRecommend(USEEFFECT).js
@@ -0,0 +1,32 @@
+function DependentEffect() {
+    const names = ["Alice", "Bob", "Charlie", "David", "Emily"];
+  
+    const [recommendations, setRecommendations] = useState([]);
+    const [searchText, setSearchText] = useState("");
+  
+    useEffect(() => {
+      // If user is not searching for anything, don't show any recomendations
+      if (searchText.length === 0) {
+        setRecommendations([]);
+      } 
+      // Else, find recommendations
+      else if (searchText.length > 0) {
+        const newRecs = names.filter((name) =>
+          name.toLowerCase().includes(searchText.toLowerCase())
+        );
+        setRecommendations(newRecs);
+      }
+    }, [searchText]);
+  
+    return (
+      <div>
+        <input type="text" onChange={(e) => setSearchText(e.target.value)} />
+        <h2>Recommendations:</h2>
+        <ul>
+          {recommendations.map((rec, index) => (
+            <li key={index}>{rec}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
---
+++ Activity Progress/Day 2/Intro To React/sampleUseEffect.js
@@ -0,0 +1,30 @@
+function LoadDataFromServer() {
+    // Create a state variable to hold the data returned from the server
+    const [data, setData] = useState("");
+    // Create a state variable to maintain loading state
+    const [loading, setLoading] = useState(false);
+  
+    async function loadData() {
+      // Set `loading` to `true` until API call returns a response
+      setLoading(true);
+  
+      // Imaginary function that performs an API call to load
+      // data from a server
+      const data = await apiCall();
+      setData(data);
+  
+      // We have the data, set `loading` to `false`
+      setLoading(false);
+    }
+  
+    // loadData is the function that is run
+    // An empty dependency array means this code is run
+    // once when the page loads
+    useEffect(() => {
+      loadData();
+    }, []);
+  
+    // Display `"Loading..."` while `loading` is `true`,
+    // otherwise display `data`
+    return <div>{loading ? "Loading..." : data}</div>;
+  }
